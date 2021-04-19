9dc406b9f7c91717ffb95dc332a51cbd39c6a22d
NenadZdravkovic97 <nenadzdravkovic97@yahoo.com>
2021-04-19 18:02:21 +0200

    Cas 9

---
+++ Cas9-JS/script.js
@@ -0,0 +1,32 @@
+// alert("Hello world!");
+// document.write("<p>Hello world!</p>");
+// console.log("Hello world!");
+// document.getElementById("para").innerHTML = "Novi sadrzaj";
+console.log(-15.27);
+console.log("Neki tekst");
+console.log('Jos jedan tekst');
+console.log("Mark's pen"); //navodnici u navodnicima
+console.log('Mark"s pen'); //navodnici u navodnicima
+console.log('Mark\'s');
+
+console.log("-15.48fsfddsgrgre94safqw9");
+
+console.log(3 + 5); // + radi kao sabiranje brojeva
+console.log("3" + "5"); // + radi kao konkatenacija stringova
+console.log(3 + "5"); // broj se konvertuje u string
+console.log(true); // Logicka vrednost true
+console.log(false); // Logicka vrednost false
+
+let x; // deklarisanje promenljive x
+x = 3; // x dobija vrednost 3
+console.log(x);
+
+
+x = 5; // dodeli novu vrednost
+console.log(x);
+
+x = "Vitez koja";
+let y = true; // deklarisanje promenljive i zadavanje pocetne vrednosti
+y = 0.5;
+console.log(x, y);
+
